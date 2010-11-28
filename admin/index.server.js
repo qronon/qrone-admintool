242f3590aeda65962226237c0517d634749a27c9
qronon <qronon@qronon-i7>
2010-11-28 12:10:54 +0900

    update

--- admin/index.server.js
+++ admin/index.server.js
@@ -1,21 +1,35 @@
 
 header("Content-Type: text/html; charset=utf8");
 
+/*
 var data = load_yaml("index.yaml");
 
 document.set(data);
-/*
 for( item in data ){
 	if(data[item] instanceof String)
 		document.set("#" + item, data[item].replace(/\n/g, "<br>") );
 }
-*/
 document.set("#point", function(doc,e){
 	for(i in data.point){
 		doc.set(".point", data.point[i]);
 		doc.out(e);
 	}
 	document.set("#point", "test");
+});
+*/
+
+var l = repository.list();
+document.set("#repolist", function(doc,e){
+	
+	if(l.length > 0){
+		for(i in data.point){
+			var b = doc.select(".repo");
+			doc.out(b);
+		}
+	}else{
+		var b = doc.select(".alert");
+		doc.out(b);
+	}
 });
 
 document.out();
