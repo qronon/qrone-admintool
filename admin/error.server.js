333f3a1580c0eb2f3c8c9da96d82c167caee8e62
qronon <qronon@qronon-i7>
2010-12-04 19:35:48 +0900

    update

--- admin/error.server.js
+++ admin/error.server.js
@@ -1,9 +1,9 @@
 
 document.set("#file", exception.file);
-document.set("#line", exception.line);
+document.set("#line", parseInt(exception.line));
 document.set("#stacktrace", exception.stacktrace);
 document.set("#source", exception.source, true);
 
 var pre = document.select("#source");
-pre.attr("class","brush: js; highlight: [" + exception.line + "]");
+pre.attr("class","brush: js; highlight: [" + parseInt(exception.line) + "]");
 document.out();
