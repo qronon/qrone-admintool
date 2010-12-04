05ceed60cb59e38e1e22f0b83e29d8be28d1b659
qronon <qronon@qronon-i7>
2010-12-04 19:08:01 +0900

    update

--- admin/error.server.js
+++ admin/error.server.js
@@ -2,5 +2,5 @@
 document.set("#file", exception.file);
 document.set("#line", exception.line);
 document.set("#stacktrace", exception.stacktrace);
-document.set("#source", exception.source);
+document.set("#source", exception.source, true);
 document.out();
