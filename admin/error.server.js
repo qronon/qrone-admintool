fb7e8d45e23fc9a00649bf2258824a7672b139b3
qronon <qronon@qronon-i7>
2010-12-04 19:14:27 +0900

    update

--- admin/error.server.js
+++ admin/error.server.js
@@ -3,4 +3,7 @@ document.set("#file", exception.file);
 document.set("#line", exception.line);
 document.set("#stacktrace", exception.stacktrace);
 document.set("#source", exception.source, true);
+
+var pre = document.select("#source");
+pre.attr("class","brush: js; highlight: [" + exception.line + "]");
 document.out();
