c18b26a9ce3383efcea85c3dea7b1cdbf2e9813e
qronon <qronon@qronon-i7>
2010-12-07 23:28:50 +0900

    update

--- admin/index.server.js
+++ admin/index.server.js
@@ -1,39 +1,9 @@
 
 header("Content-Type: text/html; charset=utf8");
 
-/*
-var data = load_yaml("index.yaml");
-
-document.set(data);
-for( item in data ){
-	if(data[item] instanceof String)
-		document.set("#" + item, data[item].replace(/\n/g, "<br>") );
-}
-document.set("#point", function(doc,e){
-	for(i in data.point){
-		doc.set(".point", data.point[i]);
-		doc.out(e);
-	}
-	document.set("#point", "test");
-});
-
-
-document.set("#repolist", function(doc,e){
-	
-	if(l.length > 0){
-		for(i in l){
-			doc.set(i);
-			doc.out($(".repo"));
-		}
-	}else{
-		doc.out($(".alert"));
-	}
-	
-});
-*/
-
 var t = load_template("main/qrone-frame.html");
 t.select("#content").html($("#frame"));
+$("#rightcolumn").html(load_template("main/sidebar.html"));
 $("body").html(t);
 
 var l = repository.list();
