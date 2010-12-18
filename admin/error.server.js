6cc23b93798b71879be602889df8d37b7139839e
qronon <qronon@qronon-i7>
2010-12-18 19:17:04 +0900

    update

--- admin/error.server.js
+++ admin/error.server.js
@@ -5,3 +5,5 @@ $("#line").html(parseInt(exception.line));
 $("#stacktrace").html(exception.stacktrace.replace(/\n/g,"<br/>"));
 $("#source").html(exception.source).attr("class",
 		"brush: js; highlight: [" + parseInt(exception.line) + "]");
+
+$("#scope").html(exception.scope);
