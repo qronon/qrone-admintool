e569413b31214a127bb8a35ce979a87581b8cd5d
qronon <qronon@qronon-i7>
2010-12-18 21:25:22 +0900

    update

---
+++ admin/bug.server.js
@@ -0,0 +1,15 @@
+
+header("Content-Type: text/html; charset=utf8");
+
+var test_array = [1,2,3];
+var test_object = {
+		id : "qronon",
+		description : "This is error page test page."
+}
+
+var t = document;
+
+bug.bug.bug;
+
+
+
