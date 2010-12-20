fddfd4f7b76ba1e5a03c7be1ad7e6bf0d71f9165
qronon <qronon@qronon-i7>
2010-12-21 02:29:02 +0900

    update

--- admin/index.server.js
+++ admin/index.server.js
@@ -11,15 +11,12 @@ if(l.length > 0){
 	var repolist = $("#repolist");
 	for(i in l){
 		var repo = $(".repo").clone();
-		for(j in l[i]){
-			$(".repo-title",repo).html(l[i].name);
-			$(".repo-owner",repo).html(l[i].owner);
+		$(".repo-title",repo).html(l[i].name);
+		$(".repo-owner",repo).html(l[i].owner);
 			
-			var repoinfo = http.get("http://github.com/api/v2/yaml/repos/show/" + l[i].owner + "/" + l[i].name);
-			repoinfo = YAML.decode(repoinfo);
-			$(".repo-body",repo).html(repoinfo.repository.description);
-			
-		}
+		var repoinfo = http.get("http://github.com/api/v2/yaml/repos/show/" + l[i].owner + "/" + l[i].name);
+		repoinfo = YAML.decode(repoinfo);
+		$(".repo-body",repo).html(repoinfo.repository.description);
 		
 		repolist.append(repo);
 	}
